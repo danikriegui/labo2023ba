51e53fe6226745048d0bda083d9c53aa52341464
danikriegui <danikriegui@gmail.com>
2023-08-21 20:46:16 +0000

    Prueba 1 de gridsearch

--- src/rpart/141_gridsearch_esqueleto.r
+++ src/rpart/141_gridsearch_esqueleto.r
@@ -138,7 +138,7 @@ for (vcp in c(-1, -0.5, 0, 0.1)) {
         ) # profundidad máxima del arbol
 
         # Un solo llamado, con la semilla 17
-        ganancia_promedio <- ArbolesMontecarlo(ksemillas, param_basicos)
+        ganancia_promedio <- ArbolesMontecarlo(PARAM$semillas, param_basicos)
 
         # escribo los resultados al archivo de salida
         cat(
